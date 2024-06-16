ef1e67969761b118b8b40bb8378ff4ea21a066b2
Figby222 <figby222@outlook.com>
2024-06-16 09:55:09 -0700

    Test if button has correct text

--- tests/CustomButton.test.jsx
+++ tests/CustomButton.test.jsx
@@ -1,3 +1,13 @@
 import { vi } from 'vitest';
 import { render, screen } from "@testing-library/react";
 import CustomButton from "../src/components/CustomButton";
+
+describe("CustomButton", () => {
+    it("should render a button with the text 'Click me'", () => {
+        render(<CustomButton onClick={() => {}} />);
+
+        const button = screen.getByRole("button", { name: "Click me"});
+
+        expect(button).toBeInTheDocument();
+    })
+})
